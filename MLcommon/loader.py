db44a43c0f7c0abbfc3470163cd2dba8f89ac4eb
juan carlos arbeláez <juanarbelaez@vaico.com.co>
2020-04-18 16:38:57 -0500

    Path working in Windows

--- MLcommon/loader.py
+++ MLcommon/loader.py
@@ -29,7 +29,8 @@ def load_zip_model(model_path, zip_path):
     sys_path.append(join('/misdoc/vaico/architectures/yolo3/zip/yolo3.zip', 'python'))
 
     importer = zipimport.zipimporter(zip_path)
-    model_module = importer.load_module('python/{0}/{0}'.format(arch_name.lower()))
+    #'python/{0}/{0}'.format(arch_name.lower())
+    model_module = importer.load_module(join('python', arch_name.lower(), arch_name.lower()))
 
     # Search architecture class name in module
     for attr in dir(model_module):
