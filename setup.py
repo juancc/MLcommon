32f3f040e428057c888e026f5748c1e7c21a28d9
juan carlos arbeláez <juanarbelaez@vaico.com.co>
2020-09-10 08:04:20 -0500

    Strategies moved to MLinference project

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
   name = 'MLcommon',
-  packages = ['MLcommon', 'MLcommon.PredictionStrategies'],
-  version = '0.1.6',
+  packages = ['MLcommon'],
+  version = '0.1.7',
   license= '',
   description = 'Common interface for Machine Learning models with explicit model protocol',
   author = 'Juan Carlos Arbelaez',
